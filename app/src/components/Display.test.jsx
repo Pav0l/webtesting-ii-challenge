1582ebbef81267bb2dbe6ae28e97c73a3a7eb84f
Pav0l <pavol.karas@gmail.com>
2019-04-09 16:17:16 +0200

    Test reset counters

--- app/src/components/Display.test.jsx
+++ app/src/components/Display.test.jsx
@@ -7,8 +7,6 @@ import Dashboard from './Dashboard';
 afterEach(rtl.cleanup);
 
 describe('Display component', () => {
-  const dashboardWrap = rtl.render(<Dashboard />);
-
   it('displays balls count', () => {
     const componentWrapper = rtl.render(<Display balls={0} />);
     expect(componentWrapper.getByText(/0/i));
@@ -21,21 +19,25 @@ describe('Display component', () => {
 
   it('increases balls count when clicking on ball button', () => {
     const displayWrap = rtl.render(<Display balls={0} />);
-
+    const dashboardWrap = rtl.render(<Dashboard />);
     expect(displayWrap.getByText(/0/i));
-
     rtl.fireEvent.click(dashboardWrap.getByText(/ball/i));
-
     expect(displayWrap.findByText(/1/i));
   });
 
   it('increases strikes count when clicking on ball button', () => {
     const displayWrap = rtl.render(<Display strikes={0} />);
-
+    const dashboardWrap = rtl.render(<Dashboard />);
     expect(displayWrap.getByText(/0/i));
-
     rtl.fireEvent.click(dashboardWrap.getByText(/strike/i));
-
     expect(displayWrap.findByText(/1/i));
+  });
+
+  it('reset strikes and balls count on hit click', () => {
+    const displayWrap = rtl.render(<Display strikes={2} balls={1} />);
+    const dashboardWrap = rtl.render(<Dashboard />);
+    expect(displayWrap.queryByText(/0/i)).toBeFalsy();
+    rtl.fireEvent.click(dashboardWrap.getByText(/hit/i));
+    expect(displayWrap.findByText(/0/i));
   });
 });
